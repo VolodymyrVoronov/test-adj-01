38c494fd8adb03ab3ae39b2f4f28624e960a88e4
Volodymyr Voronov <malones.vladimir@gmail.com>
2025-12-18 18:53:08 +0100

    fix: update base path in vite.config.ts for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/test-adj-01/" : "/",
   plugins: [
     react({
       babel: {
@@ -19,4 +20,3 @@ export default defineConfig({
     },
   },
 });
-
